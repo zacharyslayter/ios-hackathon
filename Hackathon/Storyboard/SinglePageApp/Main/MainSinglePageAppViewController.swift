2b93ac28f5dd2f3ce6902d8f01c457957cae833c
Melanie Cummings <mec948@gmail.com>
2018-07-18 19:40:28 -0500

    c

--- Hackathon/Storyboard/SinglePageApp/Main/MainSinglePageAppViewController.swift
+++ Hackathon/Storyboard/SinglePageApp/Main/MainSinglePageAppViewController.swift
@@ -8,21 +8,59 @@
 
 import Foundation
 import UIKit
+import AVFoundation
+import ApiAI
 
 class MainSinglePageAppViewController: UIViewController {
     
     @IBOutlet weak var enterText: UITextField!
     @IBAction func enterAction(_ sender: Any) {
-        let giphyViewController = UIStoryboard(name: "GiphyView", bundle: nil).instantiateInitialViewController() as? GiphyViewController
-        giphyViewController?.searchString = enterText.text!
-        if giphyViewController != nil {
-            navigationController?.pushViewController(giphyViewController!, animated: true)
+        let request = ApiAI.shared().textRequest()
+        
+        if let text = self.enterText.text, text != "" {
+            request?.query = text
+        } else {
+            return
         }
+        
+        request?.setMappedCompletionBlockSuccess({ (request, response) in
+            let response = response as! AIResponse
+            if let textResponse = response.result.fulfillment.speech {
+                self.speechAndText(text: textResponse)
+            }
+        }, failure: { (request, error) in
+            print(error!)
+        })
+        
+        ApiAI.shared().enqueue(request)
+        enterText.text = ""
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    func speechAndText(text: String) {
+        let speechSynthesizer = AVSpeechSynthesizer()
+        let speechUtterance = AVSpeechUtterance(string: text)
+        speechSynthesizer.speak(speechUtterance)
+        
+        if text == "gif_cat" {
+            let giphyViewController = UIStoryboard(name: "GiphyView", bundle: nil).instantiateInitialViewController() as? GiphyViewController
+            giphyViewController?.searchString = "cat"
+            if giphyViewController != nil {
+                navigationController?.pushViewController(giphyViewController!, animated: true)
+            }
+        }
+        
+        if text == "cam_cat" {
+            let catFactVC = UIStoryboard(name: "CatFactStoryboard", bundle: nil).instantiateInitialViewController()
+            if catFactVC != nil {
+                navigationController?.pushViewController(catFactVC!, animated: true)
+            }
+        }
+
     }
     
 
