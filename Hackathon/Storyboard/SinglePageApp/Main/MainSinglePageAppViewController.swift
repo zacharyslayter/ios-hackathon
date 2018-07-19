7d468302d27f80686a623ca9f98f94d7c1fa54d9
John Zhang <johnrongzhang@gmail.com>
2018-07-18 19:30:36 -0500

    Adding CoreML

--- Hackathon/Storyboard/SinglePageApp/Main/MainSinglePageAppViewController.swift
+++ Hackathon/Storyboard/SinglePageApp/Main/MainSinglePageAppViewController.swift
@@ -8,8 +8,13 @@
 
 import Foundation
 import UIKit
+import AVFoundation
+import Vision
+import CoreML
 
 class MainSinglePageAppViewController: UIViewController {
+    
+    @IBOutlet weak var whatIsThisResult: UILabel!
     
     @IBOutlet weak var enterText: UITextField!
     @IBAction func enterAction(_ sender: Any) {
@@ -41,9 +46,69 @@ class MainSinglePageAppViewController: UIViewController {
     }
     
     @IBAction func cameraClicked(_ sender: Any) {
-        //CameraHandler.shared.camera(forViewController: self)
-        //CameraHandler.shared.imagePickedBlock = { (image) in
-            /* get your image here */
-       // }
+        CameraHandler.shared.camera(forViewController: self)
+        CameraHandler.shared.imagePickedBlock = { (image) in
+            let model = Resnet50()
+            let buffer = self.getCVPixelBuffer(image.cgImage!)
+            
+            guard let prediction: Resnet50Output = try? model.prediction(image: buffer!) else {
+                fatalError("something something")
+            }
+            
+            var resultLabel = ""
+            if (prediction.classLabel.contains("Cat") || prediction.classLabel.contains("cat")) {
+                resultLabel = "THIS IS A CAT"
+            } else {
+                resultLabel = "THIS IS NOT A CAT THIS IS A " + prediction.classLabel;
+            }
+            
+            self.whatIsThisResult.text = resultLabel;
+        }
+    }
+    
+    func getCVPixelBuffer(_ image: CGImage) -> CVPixelBuffer? {
+        let imageWidth = 224
+        let imageHeight = 224
+        
+        let attributes : [NSObject:AnyObject] = [
+            kCVPixelBufferCGImageCompatibilityKey : true as AnyObject,
+            kCVPixelBufferCGBitmapContextCompatibilityKey : true as AnyObject
+        ]
+        
+        var pxbuffer: CVPixelBuffer? = nil
+        CVPixelBufferCreate(kCFAllocatorDefault,
+                            imageWidth,
+                            imageHeight,
+                            kCVPixelFormatType_32ARGB,
+                            attributes as CFDictionary?,
+                            &pxbuffer)
+        
+        if let _pxbuffer = pxbuffer {
+            let flags = CVPixelBufferLockFlags(rawValue: 0)
+            CVPixelBufferLockBaseAddress(_pxbuffer, flags)
+            let pxdata = CVPixelBufferGetBaseAddress(_pxbuffer)
+            
+            let rgbColorSpace = CGColorSpaceCreateDeviceRGB();
+            let context = CGContext(data: pxdata,
+                                    width: imageWidth,
+                                    height: imageHeight,
+                                    bitsPerComponent: 8,
+                                    bytesPerRow: CVPixelBufferGetBytesPerRow(_pxbuffer),
+                                    space: rgbColorSpace,
+                                    bitmapInfo: CGImageAlphaInfo.premultipliedFirst.rawValue)
+            
+            if let _context = context {
+                _context.draw(image, in: CGRect.init(x: 0, y: 0, width: imageWidth, height: imageHeight))
+            }
+            else {
+                CVPixelBufferUnlockBaseAddress(_pxbuffer, flags);
+                return nil
+            }
+            
+            CVPixelBufferUnlockBaseAddress(_pxbuffer, flags);
+            return _pxbuffer;
+        }
+        
+        return nil
     }
 }
